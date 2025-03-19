fd950823eadf2e39f6219cbb110a6a8934443ac2
Sk Asif Iqbal <skasifiqbal786@gmail.com>
2025-03-19 05:36:48 +0530

    checking ECS ci cd

--- src/main/java/com/asifiqbalsekh/EcomBE/controller/AuthController.java
+++ src/main/java/com/asifiqbalsekh/EcomBE/controller/AuthController.java
@@ -167,7 +167,7 @@ public class AuthController {
 
         return ResponseEntity.ok().body(
                 new SignUpResponseDTO(
-                        "test","test@email.com","CI-CD Check ok"
+                        "test","test@email.com","CI-CD ECS Check - Congratulations Working fine!"
                 )
         );
     }
